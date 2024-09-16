da2e3132b2fea5997a333443df250d2c100e3da9
Dwaipayan <biswastatay73@gmail.com>
2024-09-16 13:04:11 +0530

    Update

---
+++ server.js
@@ -0,0 +1,18 @@
+console.log("Dwaipayan");
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// var add = function(a,b){
+//     return a+b;
+// }
+
+// var add = (a, b) => {
+//   return a + b;
+// };
+
+var add = (a, b) => a + b;
+
+let result = add(2, 11);
+console.log(result);
