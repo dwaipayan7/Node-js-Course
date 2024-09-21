dec783de610a8657f0687cf10f996914e70c8630
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-09-21 13:06:24 +0530

    Updated connection

--- run.server.js
+++ run.server.js
@@ -15,6 +15,32 @@ app.get("/", function (req, res) {
   res.send("Welcome to my Hotel");
 });
 
+app.post('/person', (req, res)=>{
+
+  const data = res.body;
+
+  const newPerson = new Person(data); //prefilled with database
+
+  newPerson.sava((error, savedPerson) =>{
+    if (error) {
+      console.log('Error Saving Person', error);
+      res.status(500).json({error: 'Internal server Error'});
+
+    }else{
+      console.log("Data saved Successfully");
+      res.status(200).json(savedPerson);
+    }
+  });
+
+  // newPerson.name = data.name;
+  // newPerson.age = data.age;
+  // newPerson.mobile = data.mobile;
+  // newPerson.email = data.email;
+  // newPerson.address = data.address;
+
+
+});
+
 // app.get("/tatay", function (req, res) {
 //   res.send("Dwaipayan ");
 // });
