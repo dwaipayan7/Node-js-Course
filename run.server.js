219f57990e8d9d0efab6fe9d4598fd722f6d6ade
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-09-26 15:38:38 +0530

    Verification with passport authentication

--- run.server.js
+++ run.server.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 const app = express();
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 const db = require("./db"); // Assuming you have a proper database connection file
+
+const Person = require('./models/Person.js');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // Parse JSON body
@@ -18,12 +23,41 @@ const logRequest = (req, res, next) => {
 //   res.send("Welcome to my Hotel");
 // });
 
+
+
 // Person and MenuItem models
 const Person = require('./models/Person');
 const MenuItem = require('./models/MenuItem');
 
 // Use logRequest middleware for all routes
 app.use(logRequest);
+
+
+app.use(new LocalStrategy(async (username, password, done) =>{
+
+  try {
+      console.log('Received credential:',username, password);
+
+      const user = Person.findOne({username: username});
+    
+      if (!user) 
+        return done(null, false, {message: 'Incorrect Username'});
+
+        const isPasswordMatch = user.password === password ?
+        true : false;
+
+        if (isPasswordMatch) {
+          return done(null, user);
+        }else{
+          return done(null, false, {message: 'Incorrect Password'});
+        }
+
+
+  } catch (error) {
+    return done(error);
+  }
+
+}));
 
 // Route for testing the root '/' route
 app.get('/', function (req, res) {
